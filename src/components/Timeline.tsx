06d6a733da6d67a3e8c655cc813adf9362d08c96
Manoowranjith <a.j.manoow@gmail.com>
2024-10-31 20:34:09 +0530

    rm unused code

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
@@ -6,15 +5,8 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
-import LaptopMacIcon from '@mui/icons-material/LaptopMac';
-import HotelIcon from '@mui/icons-material/Hotel';
 
 import Typography from '@mui/material/Typography';
-import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
-
-import SchoolIcon from '@mui/icons-material/School';
-
 export default function CustomizedTimeline() {
     return (
         <Timeline position="alternate">
